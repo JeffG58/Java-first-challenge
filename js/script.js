a8d82c245f428ab9bae3f98acaae99d0b8796c99
Jeff Grismore <jeffgrismore24@yahoo.com>
2024-06-26 19:29:47 -0400

    cat picture shows up with button push

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+button.addEventListener("click", function(){
+    button.innerText = "It's Working!"
+    cat.classList.add("show");
+});
