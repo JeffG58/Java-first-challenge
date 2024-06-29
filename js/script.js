f767d251f7b8c442628f3f81d11cfd257920cc33
Jeff G <jeffgrismore24@yahoo.com>
2024-06-29 14:42:17 -0400

    kitty challenge 2 done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 button.addEventListener("click", function(){
-    button.innerText = "It's Working!"
+    //button.innerText = "It's Working!"
+   if (cat.classList.contains("show")) {
+
+   cat.classList.remove("show");
+   button.innerText = "Wait, come back!"
+   button.classList.add("disappear");
+
+   }else{
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!"
+    button.classList.remove("disappear");
+}
 });
